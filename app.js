3260dba228acaa3cffacd64ab36407bdeab91119
Rachel <rmshinski@gmail.com>
2021-04-13 16:33:10 -0400

    homework

---
+++ app.js
@@ -0,0 +1,23 @@
+function addTwo (x, y) {
+    answer = x+y;
+    console.log (answer);
+}
+addTwo(2,4);
+
+function minusThis (a, b) {
+    answer = b-a;
+    console.log (answer);
+}
+minusThis(2,6);
+
+function divThis (c,d) {
+    answer = c/d;
+    console.log (answer);
+}
+divThis (10,5);
+
+function multThis (e,f) {
+    answer = e * f;
+    console.log (answer);
+}
+multThis (25,4)
